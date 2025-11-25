e129127beea4f0c53afb745c18fc76b96f8a8d1e
nicholasf <nicholas.faiz@gmail.com>
2025-11-25 11:01:23 +1100

    Generics, defer and recover - go Leetcode - LRU cache typescript

---
+++ node/ts/leetcode/lru-cache/jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+};
